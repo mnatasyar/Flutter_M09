4c7216da00172abcc089164139386d72e39eb8bc
M Natasya Ramadana <93390884+mnatasyar@users.noreply.github.com>
2023-05-22 16:28:43 +0700

    Add files via upload

---
+++ lib/pertemuan09/pertemuan09_screen.dart
@@ -0,0 +1,164 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Pertemuan09Screen extends StatefulWidget {
+  const Pertemuan09Screen({super.key});
+
+  @override
+  State<Pertemuan09Screen> createState() => _Pertemuan09ScreenState();
+}
+
+class _Pertemuan09ScreenState extends State<Pertemuan09Screen> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Pertemuan 09"),
+          bottom: TabBar(isScrollable: true, tabs: [
+            Tab(
+              child: Text('Music'),
+            ),
+            Tab(
+              child: Text('Favorite'),
+            ),
+            Tab(
+              child: Text('Saved'),
+            ),
+          ]),
+        ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              DrawerHeader(
+                padding: EdgeInsets.all(16.0),
+                decoration: BoxDecoration(color: Colors.purple),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    CircleAvatar(
+                      child: Image.network(
+                          "https://clipartcraft.com/images/twitter-logo-png-round-3.png"),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 16),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Joen Doe',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            'joen@mail.com',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Divider(),
+              ListTile(
+                onTap: () {},
+                leading: Icon(Icons.inbox),
+                title: Text('Inbox'),
+                trailing: Icon(Icons.keyboard_arrow_right_outlined),
+              ),
+              ListTile(
+                onTap: () {},
+                leading: Icon(Icons.save),
+                title: Text('Archived'),
+                trailing: Icon(Icons.keyboard_arrow_right_outlined),
+              ),
+              ListTile(
+                onTap: () {},
+                leading: Icon(Icons.download),
+                title: Text('Saved'),
+                trailing: Icon(Icons.keyboard_arrow_right_outlined),
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            buttonSheets(context),
+            // TextButton(
+            //   onPressed: () {
+            //     print('Home');
+            //   },
+            //   child: Text('Home'),
+            // ),
+            TextButton(
+              onPressed: () {
+                print('Favorite');
+              },
+              child: Text('Favorite'),
+            ),
+            TextButton(
+              onPressed: () {
+                print('Saved');
+              },
+              child: Text('Saved'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+buttonSheets(BuildContext context) {
+  return Builder(builder: ((context) {
+    return Center(
+        child: TextButton(
+            onPressed: () {
+              showBottomSheet(
+                enableDrag: true,
+                context: context,
+                builder: (context) {
+                  final theme = Theme.of(context);
+                  return Wrap(
+                    children: [
+                      ListTile(
+                        title: const Text(
+                          'Header',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 17,
+                              color: Colors.white),
+                        ),
+                        tileColor: theme.colorScheme.primary,
+                        trailing: IconButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          icon: Icon(
+                            Icons.close,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      ListTile(
+                        title: Text('Facebook'),
+                      ),
+                      ListTile(
+                        title: Text('Twitter'),
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
+            child: Text('Social Media Share')));
+  }));
+}
